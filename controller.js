4e616a3efe28aa766e2ecca6cb8c335344593972
Dudi Lugasi <dulu123@gmail.com>
2015-06-14 12:38:51 +0300

    added blocked recipes filter

--- controller.js
+++ controller.js
@@ -15,7 +15,7 @@ var Recipes = mongoose.model('recipesM');
 var Ingredients = mongoose.model('ingredientsM');
 var Users = mongoose.model('usersM');
 
-
+//returns the likes array of a user
 exports.getUserLikes = function(req,res) {
     var userID =  url.parse(req.url,true).query.user_id;
     if (userID) {
@@ -32,15 +32,11 @@ exports.getUserLikes = function(req,res) {
     }
 };
 
+//update the likes array of a user
 exports.putUserLikes = function(req,res) {
     var userID = url.parse(req.url,true).query.user_id;
     var likes = url.parse(req.url,true).query.likes;
-    if (!likes) {
-        likes = []
-    }
-    else if (!Array.isArray(likes)) {
-        likes = [likes];
-    }
+    likes = makeAnArray(likes);
     console.log(likes);
     Users.update({user_id: userID},{likes: likes}).exec(function(err,data){
         Users.findOne().where('user_id').equals(userID).exec(function (err, data) {
@@ -51,22 +47,22 @@ exports.putUserLikes = function(req,res) {
     });
 };
 
+//receives an array of ingredients and an array of blocked recipes
+//and returns all the recipes that do not have the ingredients and not a blocked recipe
 exports.getRecipes = function(req,res) {
     var ingredients = url.parse(req.url,true).query.ingredients;
-    if (!ingredients) {
-        ingredients = []
-    }
-    else if (!Array.isArray(ingredients)) {
-        ingredients = [ingredients];
-    }
-    console.log(ingredients);
-    Recipes.find().where('ingredients.name').nin(ingredients).exec(function(err,data){
+    var blockedRecipes = url.parse(req.url,true).query.blockedRecipes;
+    ingredients = makeAnArray(ingredients);
+    blockedRecipes = makeAnArray(blockedRecipes);
+    console.log(ingredients,blockedRecipes);
+    Recipes.find().where('ingredients.name').nin(ingredients).where('name').nin(blockedRecipes).exec(function(err,data){
         res.header('Access-Control-Allow-Origin', '*');
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.status(200).json(data);
     });
 };
 
+//get all the ingredients from the database
 exports.getIngredients = function(req,res) {
     Ingredients.find().exec(function(err,data){
         res.header('Access-Control-Allow-Origin', '*');
@@ -74,3 +70,15 @@ exports.getIngredients = function(req,res) {
         res.status(200).json(data[0].items);
     });
 };
+
+function makeAnArray(array) {
+    //if the parameter is empty it will be empty array
+    if (!array) {
+        array = []
+    }
+    //if the parameter is not an array it will be an array with one element
+    else if (!Array.isArray(array)) {
+        array = [array];
+    }
+    return array;
+}
--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('/get-likes',controller.getUserLikes);
 
 app.get('/add-likes',controller.putUserLikes);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8000);
 
 console.log('listening');
